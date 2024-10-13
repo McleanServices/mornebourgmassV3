2c2141cc371ce6ac9a54677521e12ca26c586afd
TyreceMclean <tyrecemcleanterence@gmail.com>
2024-10-13 12:02:14 -0400

    Refactor navigation structure and add bottom tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import React from 'react';
 import Welcome from './assets/screens/Welcome';
 import Login from './assets/screens/Login';
+import HomeScreen from './assets/screens/HomeScreen';
+import Profile from './assets/screens/Profile';
+import SettingsScreen from './assets/screens/Settings';
+import RegisterScreen from './assets/screens/Register';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#ffd33d', // Custom active tab color
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={({ navigation }) => ({ // Access navigation here
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'person-sharp' : 'person-outline'} color={color} size={24} />
+          ),
+          headerRight: () => (
+            <Ionicons 
+              name="settings-outline" // Use 'settings-sharp' for filled version
+              size={24}
+              style={{ marginRight: 10 }} // Add some margin to the right
+              onPress={() => {
+                navigation.navigate('Settings'); // Use the navigation prop here
+                console.log('Settings pressed');
+              }}
+            />
+          ),
+        })}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -14,7 +61,11 @@ export default function App() {
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={BottomTabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
+      <StatusBar />
     </NavigationContainer>
   );
 }
